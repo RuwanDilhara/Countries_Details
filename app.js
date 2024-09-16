f5d5a8efa42734557c01055ba7f18cfebd7e366a
RuwanDilhara <roshansameera916@gmail.com>
2024-09-16 11:46:26 +0530

    add map link

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ function search() {
     let searchValue = document.getElementById("txtSearch").value;
     let tblCountries = document.getElementById("tblCountries");
     let exampleimg = document.getElementById("example");
+    let maplink = document.getElementById("map");
+    let locattion_header = document.getElementById("locationh1");
+
+    let trueLocation;
     let tblBody = `<tr>
                         <th class="th">Name</th>
                         <th class="th">Flag</th>
@@ -22,9 +26,19 @@ function search() {
                                 <td class="td">${element.region}</td>
                                 <td class="td">${element.population}</td>
                             </tr>`
+                            console.log(element.maps.googleMaps);
+                locattion_header.innerHTML="Location"
+                maplink.innerHTML=`<a href="${element.maps.googleMaps}"> ${searchValue} Location <=click_here</a>`
+
+
             });
             tblCountries.innerHTML = tblBody;
             exampleimg.hidden=true;
-            console.log(exampleimg);
+            
+            
+
+            
+            // map.innerHTML= trueLocation;
+
         });
 }
